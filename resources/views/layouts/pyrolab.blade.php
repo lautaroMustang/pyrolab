cbdd558a2ca448c1e6418a848a6027bfff4e8f2f
lautaromustang <lautarogonzalez@outlook.com>
2019-02-12 10:59:16 -0300

    inicio Home

---
+++ resources/views/layouts/pyrolab.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> {{-- CHEQUEAR --}}
+
+    <title>@yield('htmlHeaderTitle', 'Pyrolab - Home')</title>
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
+</html>
